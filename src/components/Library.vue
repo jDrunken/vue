dc67e13da7b5e74c31aabe6106b198120771a685
drunken_j <mycoolade@gmail.com>
2018-11-22 03:06:18 +0900

    static html chunk를 ajax 처리로 변경함

--- src/components/Library.vue
+++ src/components/Library.vue
@@ -6,55 +6,26 @@
 	</ul> <!-- //.nav-section -->
 
 	<table class="table library-table">
-		<colgroup><col width="95">
-		</colgroup><tbody>
-
-			<tr>
+		<colgroup>
+			<col width="95">
+		</colgroup>
+		<tbody>
+			<tr v-for="list in library">
 				<td>
-					<a href="/classicalinsight/LOTUS0T" class="thumb" onfocus="this.blur()">
-						<img data-src="/images/etc/bnr-dummy-album.jpg" alt="Classical Vol. 32 : 바흐, 라흐마니노프" class="img-responsive lazy-loaded" src="/images/etc/bnr-dummy-album.jpg">
+					<a v-bind:href="list.href" class="thumb">
+						<img src="/images/etc/bnr-dummy-album.jpg" alt="" class="img-responsive">
 						<i></i>
 					</a>
 				</td>
 				<td>
-					<!-- Classical Insight -->
-					<a href="/classicalinsight/LOTUS0T" onfocus="this.blur()">
-						<h4>
-							Groovers Pick
-							- Classical Vol. 32 : 바흐, 라흐마니노프</h4>
+					<a v-bind:href="list.href">
+						<h4>{{list.title}}</h4>
 					</a>
-					<a href="/classicalinsight/LOTUS0T" onfocus="this.blur()">
-						<p class="ell3">하프시코드 같은 느낌을 내기 위해 한 옥타브씩 올리거나 내리곤 하던 이전과 달리 음색이나 명암, 아티큘레이션에 미묘한 변화를 준 시프의 선택은 현명했다. 이 모든 것이 전면적으로 드러나지 않고 즉흥연주와도 같이 돌발적으로, 하지만 바흐작품의 구조를 파괴하지 않는 선에서 시의적절하게 등장하기에 음악은 생동감과 안정감을 동시에 달성하는데, 이 곡이 철저한 계산에 의한 변주곡임을 염두에 두면 이상적인 해석이 아닐 수 없다.</p>
+					<a v-bind:href="list.href">
+						<p>{{list.desc}}</p>
 					</a>
 				</td>
 			</tr>
-			<tr>
-				<td>
-					<a href="/column/LOTUSTV" class="thumb" onfocus="this.blur()">
-						<img data-src="/images/etc/bnr-dummy-album.jpg" alt="Groovers' Speical : 자유와 열정을 노래한 음악, Reggae" class="img-responsive" src="/images/etc/bnr-dummy-album.jpg">
-						<i></i>
-					</a>
-				</td>
-				<td>
-					<!-- Column -->
-					<a href="/column/LOTUSTV" onfocus="this.blur()"><h4>Groovers' Speical : 자유와 열정을 노래한 음악, Reggae</h4></a>
-					<a href="/column/LOTUSTV" onfocus="this.blur()"><p class="ell3">레게는 1960년대 자메이카에서 스카와 록 스테디를 기반으로 태동한 음악이다. 1960년대 미국에서 블루스가 소울로 변화하는 시점에 발맞추어 새로운 스타일로 발전하게 된다. </p></a>
-				</td>
-			</tr>
-			<tr>
-				<td>
-					<a href="/linernote/LOT6nwq/LOTUSsC" class="thumb" onfocus="this.blur()">
-						<img data-src="/artwork/00/0/0/11/94/0000119445.jpg" alt="" class="img-responsive lazy-loaded" src="/images/etc/bnr-dummy-album.jpg">
-						<i></i>
-					</a>
-				</td>
-				<td>
-					<!-- Liner Note -->
-					<a href="/linernote/LOT6nwq/LOTUSsC" onfocus="this.blur()"><h4>Chris Botti, [Italia]</h4></a>
-					<a href="/linernote/LOT6nwq/LOTUSsC" onfocus="this.blur()"><p class="ell3">크리스 보티가 새롭게 선보인 이번 앨범 [Italia]가 담고 있는 두 개의 대주제는 ‘이탈리아’와 ‘로맨스’이다. 아니, 어찌 보면 그에게 있어 이탈리아는 ‘로맨스’ 그 자체인지도 모른다. </p></a>
-				</td>
-			</tr>
-
 		</tbody>
 	</table>
 </div> <!-- //.col-xs-7 .col-634px -->
@@ -62,6 +33,23 @@
 
 <script>
 export default {
-	name : 'Library'
+	name : 'Library',
+	data () {
+		return {
+			library : []
+		}
+	},
+	methods : {
+		getLibrary : function () {
+			this.$http.get('http://localhost:3000/library')
+				.then((response) => {
+					this.library = response.data;
+					console.log(this.library);
+				})
+		}
+	},
+	mounted () {
+		this.getLibrary();
+	}
 }
 </script>
