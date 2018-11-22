98e0b36f01093e001412c4657352df0765e62b80
Jinhun Shin <jinhun@chainpartners.net>
2018-11-22 11:28:27 +0900

    focuson 모듈화, ajax 처리.

--- src/components/FocusOn.vue
+++ src/components/FocusOn.vue
@@ -2,17 +2,50 @@
 <div class="col-xs-5 col-342px">
 	<ul class="nav nav-section">
 		<li><h3>Focus On!</h3></li>
-		<li class="arrow" id="hot-item"><a class="btn btn-icon btn-arrow-left-gray" data-exec="prev" onfocus="this.blur()"></a><a class="btn btn-icon btn-arrow-right-gray" data-exec="next" onfocus="this.blur()"></a></li>
+		<li class="arrow" id="hot-item" v-if="viewMoreButtonGroup">
+			<a class="btn btn-icon btn-arrow-left-gray" data-exec="prev" onfocus="this.blur()">
+			</a>
+			<a class="btn btn-icon btn-arrow-right-gray" data-exec="next">
+			</a>
+		</li>
 	</ul> <!-- //.nav-section -->
 
-	<div class="item-container active">
+	<div class="item-container active" v-bind:key="list.id" v-for="list in lists">
 		<div class="item-img">
-			<a href="/user/vipLounge" onfocus="this.blur()">
-				<img src="/images/gallery/0000002411.jpg" alt="" class="img-responsive">
+			<a v-bind:href="list.href">
+				<img v-bind:src="list.img" alt="" class="img-responsive">
 			</a>
 		</div>
-		<span class="item-title ell" title="Welcome! 그루버스 VIP!!">Welcome! 그루버스 VIP!!</span>
-		<strong class="item-price ell" title="2018년, 그루버스 VIP 멤버십을 소개합니다!">2018년, 그루버스 VIP 멤버십을 소개합니다!</strong>
+		<span class="item-title ell" v-bind:title="list.title">{{ list.title }}</span>
+		<strong class="item-price ell" v-bind:title="list.desc">{{ list.desc }}</strong>
 	</div>
 </div>
 </template>
+
+<script>
+export default {
+	name : 'FocusOn',
+	data : function () {
+		return {
+			lists : []
+		}
+	},
+	computed : {
+		viewMoreButtonGroup : function () {
+			let _length = this.lists.length;
+			return _length <= 1 ? false : true
+		}
+	},
+	methods : {
+		getList : function () {
+			this.$http.get('http://localhost:3000/focuson')
+				.then((response) => {
+					this.lists = response.data;
+				})
+		}
+	},
+	mounted : function () {
+		this.getList();
+	},
+}
+</script>
